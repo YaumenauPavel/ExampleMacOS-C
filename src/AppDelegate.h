ce0e2c2912f21fc230170929431a446b7c1e3e77
Yaumenau Pavel <yaumenau.pavel@planetvpn.dev>
2023-11-27 23:46:59 +0300

    init project obj-c

---
+++ src/AppDelegate.h
@@ -0,0 +1,10 @@
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
